339ecb48392545e6e8ce47d9b9eb895468c48109
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-04-16 17:58:42 +0200

    Fix the EditVegSignScreen

--- src/main/java/com/cifru/additionalblocks/vegetation/BordItem.java
+++ src/main/java/com/cifru/additionalblocks/vegetation/BordItem.java
@@ -6,8 +6,9 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.SignItem;
+import net.minecraft.item.WallOrFloorItem;
 import net.minecraft.tileentity.SignTileEntity;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -15,19 +16,23 @@ import net.minecraft.world.World;
 import javax.annotation.Nullable;
 import java.util.function.Supplier;
 
-public class BordItem extends SignItem implements IConfigObject {
+public class BordItem extends WallOrFloorItem implements IConfigObject {
     private final Supplier<Boolean> enable;
 
     public BordItem(String registryName, Supplier<Boolean> configValue, Properties propertiesIn, Block floorBlockIn, Block wallBlockIn) {
-        super(propertiesIn, floorBlockIn, wallBlockIn);
+        super(floorBlockIn, wallBlockIn, propertiesIn);
         this.setRegistryName(registryName);
         this.enable = configValue;
     }
 
     protected boolean onBlockPlaced(BlockPos pos, World worldIn, @Nullable PlayerEntity player, ItemStack stack, BlockState state) {
         boolean flag = super.onBlockPlaced(pos, worldIn, player, stack, state);
-        if (!worldIn.isRemote && !flag && player != null) {
-            Minecraft.getInstance().displayGuiScreen(new EditVegSignScreen((SignTileEntity) worldIn.getTileEntity(pos)));
+        TileEntity tile = worldIn.getTileEntity(pos);
+        if (tile instanceof SignTileEntity && !flag && player != null) {
+            if(worldIn.isRemote)
+                Minecraft.getInstance().displayGuiScreen(new EditVegSignScreen((SignTileEntity) tile));
+            else
+                ((SignTileEntity)tile).setPlayer(player);
         }
 
         return flag;
--- src/main/java/com/cifru/additionalblocks/vegetation/EditVegSignScreen.java
+++ src/main/java/com/cifru/additionalblocks/vegetation/EditVegSignScreen.java
@@ -8,109 +8,130 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.StandingSignBlock;
 import net.minecraft.client.gui.DialogTexts;
 import net.minecraft.client.gui.fonts.TextInputUtil;
-import net.minecraft.client.gui.screen.EditSignScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.network.play.ClientPlayNetHandler;
 import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.model.RenderMaterial;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.tileentity.SignTileEntityRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.network.play.client.CUpdateSignPacket;
 import net.minecraft.tileentity.SignTileEntity;
 import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 
 import java.util.stream.IntStream;
 
-public class EditVegSignScreen extends EditSignScreen {
+public class EditVegSignScreen extends Screen {
 
-    private final SignTileEntity tileSign;
     private final SignTileEntityRenderer.SignModel signModel = new SignTileEntityRenderer.SignModel();
+    /**
+     * Reference to the sign object.
+     */
+    private final SignTileEntity tileSign;
+    /**
+     * Counts the number of screen updates.
+     */
     private int updateCounter;
+    /**
+     * The index of the line that is being edited.
+     */
     private int editLine;
     private TextInputUtil textInputUtil;
     private final String[] field_238846_r_;
 
-    public EditVegSignScreen(SignTileEntity teSign) {
-        super(teSign);
-        this.tileSign = teSign;
+    public EditVegSignScreen(SignTileEntity teSign){
+        super(new TranslationTextComponent("sign.edit"));
         this.field_238846_r_ = IntStream.range(0, 4).mapToObj(teSign::getText).map(ITextComponent::getString).toArray(String[]::new);
+        this.tileSign = teSign;
     }
 
-    public boolean charTyped(char codePoint, int modifiers) {
+    protected void init(){
+        this.minecraft.keyboardListener.enableRepeatEvents(true);
+        this.addButton(new Button(this.width / 2 - 100, this.height / 4 + 120, 200, 20, DialogTexts.GUI_DONE, (p_238847_1_) -> this.close()));
+        this.tileSign.setEditable(false);
+        this.textInputUtil = new TextInputUtil(() -> this.field_238846_r_[this.editLine], (p_238850_1_) -> {
+            this.field_238846_r_[this.editLine] = p_238850_1_;
+            this.tileSign.setText(this.editLine, new StringTextComponent(p_238850_1_));
+        }, TextInputUtil.getClipboardTextSupplier(this.minecraft), TextInputUtil.getClipboardTextSetter(this.minecraft), (p_238848_1_) -> this.minecraft.fontRenderer.getStringWidth(p_238848_1_) <= 90);
+    }
+
+    public void onClose(){
+        this.minecraft.keyboardListener.enableRepeatEvents(false);
+        ClientPlayNetHandler clientplaynethandler = this.minecraft.getConnection();
+        if(clientplaynethandler != null){
+            clientplaynethandler.sendPacket(new CUpdateSignPacket(this.tileSign.getPos(), this.field_238846_r_[0], this.field_238846_r_[1], this.field_238846_r_[2], this.field_238846_r_[3]));
+        }
+
+        this.tileSign.setEditable(true);
+    }
+
+    public void tick(){
+        ++this.updateCounter;
+        if(!this.tileSign.getType().isValidBlock(this.tileSign.getBlockState().getBlock())){
+            this.close();
+        }
+
+    }
+
+    private void close(){
+        this.tileSign.markDirty();
+        this.minecraft.displayGuiScreen(null);
+    }
+
+    public boolean charTyped(char codePoint, int modifiers){
         this.textInputUtil.putChar(codePoint);
         return true;
     }
 
-    private void close() {
-        this.tileSign.markDirty();
-        this.minecraft.displayGuiScreen((Screen)null);
+    public void closeScreen(){
+        this.close();
     }
 
-    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        if (keyCode == 265) {
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers){
+        if(keyCode == 265){
             this.editLine = this.editLine - 1 & 3;
             this.textInputUtil.moveCursorToEnd();
             return true;
-        } else if (keyCode != 264 && keyCode != 257 && keyCode != 335) {
+        }else if(keyCode != 264 && keyCode != 257 && keyCode != 335){
             return this.textInputUtil.specialKeyPressed(keyCode) || super.keyPressed(keyCode, scanCode, modifiers);
-        } else {
+        }else{
             this.editLine = this.editLine + 1 & 3;
             this.textInputUtil.moveCursorToEnd();
             return true;
         }
     }
 
-    protected void init() {
-        super.init();
-        this.textInputUtil = new TextInputUtil(() -> {
-            return this.field_238846_r_[this.editLine];
-        }, (p_238850_1_) -> {
-            this.field_238846_r_[this.editLine] = p_238850_1_;
-            this.tileSign.setText(this.editLine, new StringTextComponent(p_238850_1_));
-        }, TextInputUtil.getClipboardTextSupplier(this.minecraft), TextInputUtil.getClipboardTextSetter(this.minecraft), (p_238848_1_) -> {
-            return this.minecraft.fontRenderer.getStringWidth(p_238848_1_) <= 90;
-        });
-    }
-
-    @Override
-    public void tick() {
-        super.tick();
-        this.updateCounter++;
-    }
-
-    @Override
-    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks){
         RenderHelper.setupGuiFlatDiffuseLighting();
         this.renderBackground(matrixStack);
         drawCenteredString(matrixStack, this.font, this.title, this.width / 2, 40, 16777215);
         matrixStack.push();
-        matrixStack.translate((double)(this.width / 2), 0.0D, 50.0D);
-        float f = 93.75F;
+        matrixStack.translate(this.width / 2d, 0.0D, 50.0D);
         matrixStack.scale(93.75F, -93.75F, 93.75F);
         matrixStack.translate(0.0D, -1.3125D, 0.0D);
         BlockState blockstate = this.tileSign.getBlockState();
         boolean flag = blockstate.getBlock() instanceof StandingSignBlock;
-        if (!flag) {
+        if(!flag){
             matrixStack.translate(0.0D, -0.3125D, 0.0D);
         }
 
         boolean flag1 = this.updateCounter / 6 % 2 == 0;
-        float f1 = 0.6666667F;
         matrixStack.push();
         matrixStack.scale(0.6666667F, -0.6666667F, -0.6666667F);
-        IRenderTypeBuffer.Impl irendertypebuffer$impl = this.minecraft.getRenderTypeBuffers().getBufferSource();
+        IRenderTypeBuffer.Impl renderbuffer$impl = this.minecraft.getRenderTypeBuffers().getBufferSource();
         RenderMaterial rendermaterial = BordTileEntityRenderer.getMaterial(blockstate.getBlock());
-        IVertexBuilder ivertexbuilder = rendermaterial.getBuffer(irendertypebuffer$impl, this.signModel::getRenderType);
+        IVertexBuilder ivertexbuilder = rendermaterial.getBuffer(renderbuffer$impl, this.signModel::getRenderType);
         this.signModel.signBoard.render(matrixStack, ivertexbuilder, 15728880, OverlayTexture.NO_OVERLAY);
-        if (flag) {
+        if(flag){
             this.signModel.signStick.render(matrixStack, ivertexbuilder, 15728880, OverlayTexture.NO_OVERLAY);
         }
 
         matrixStack.pop();
-        float f2 = 0.010416667F;
-        matrixStack.translate(0.0D, (double)0.33333334F, (double)0.046666667F);
+        matrixStack.translate(0.0D, 0.33333334F, 0.046666667F);
         matrixStack.scale(0.010416667F, -0.010416667F, 0.010416667F);
         int i = this.tileSign.getTextColor().getTextColor();
         int j = this.textInputUtil.getEndIndex();
@@ -118,37 +139,37 @@ public class EditVegSignScreen extends EditSignScreen {
         int l = this.editLine * 10 - this.field_238846_r_.length * 5;
         Matrix4f matrix4f = matrixStack.getLast().getMatrix();
 
-        for(int i1 = 0; i1 < this.field_238846_r_.length; ++i1) {
+        for(int i1 = 0; i1 < this.field_238846_r_.length; ++i1){
             String s = this.field_238846_r_[i1];
-            if (s != null) {
-                if (this.font.getBidiFlag()) {
+            if(s != null){
+                if(this.font.getBidiFlag()){
                     s = this.font.bidiReorder(s);
                 }
 
                 float f3 = (float)(-this.minecraft.fontRenderer.getStringWidth(s) / 2);
-                this.minecraft.fontRenderer.func_238411_a_(s, f3, (float)(i1 * 10 - this.field_238846_r_.length * 5), i, false, matrix4f, irendertypebuffer$impl, false, 0, 15728880, false);
-                if (i1 == this.editLine && j >= 0 && flag1) {
-                    int j1 = this.minecraft.fontRenderer.getStringWidth(s.substring(0, Math.max(Math.min(j, s.length()), 0)));
+                this.minecraft.fontRenderer.func_238411_a_(s, f3, (float)(i1 * 10 - this.field_238846_r_.length * 5), i, false, matrix4f, renderbuffer$impl, false, 0, 15728880, false);
+                if(i1 == this.editLine && j >= 0 && flag1){
+                    int j1 = this.minecraft.fontRenderer.getStringWidth(s.substring(0, Math.min(j, s.length())));
                     int k1 = j1 - this.minecraft.fontRenderer.getStringWidth(s) / 2;
-                    if (j >= s.length()) {
-                        this.minecraft.fontRenderer.func_238411_a_("_", (float)k1, (float)l, i, false, matrix4f, irendertypebuffer$impl, false, 0, 15728880, false);
+                    if(j >= s.length()){
+                        this.minecraft.fontRenderer.func_238411_a_("_", (float)k1, (float)l, i, false, matrix4f, renderbuffer$impl, false, 0, 15728880, false);
                     }
                 }
             }
         }
 
-        irendertypebuffer$impl.finish();
+        renderbuffer$impl.finish();
 
-        for(int i3 = 0; i3 < this.field_238846_r_.length; ++i3) {
+        for(int i3 = 0; i3 < this.field_238846_r_.length; ++i3){
             String s1 = this.field_238846_r_[i3];
-            if (s1 != null && i3 == this.editLine && j >= 0) {
-                int j3 = this.minecraft.fontRenderer.getStringWidth(s1.substring(0, Math.max(Math.min(j, s1.length()), 0)));
+            if(s1 != null && i3 == this.editLine && j >= 0){
+                int j3 = this.minecraft.fontRenderer.getStringWidth(s1.substring(0, Math.min(j, s1.length())));
                 int k3 = j3 - this.minecraft.fontRenderer.getStringWidth(s1) / 2;
-                if (flag1 && j < s1.length()) {
+                if(flag1 && j < s1.length()){
                     fill(matrixStack, k3, l - 1, k3 + 1, l + 9, -16777216 | i);
                 }
 
-                if (k != j) {
+                if(k != j){
                     int l3 = Math.min(j, k);
                     int l1 = Math.max(j, k);
                     int i2 = this.minecraft.fontRenderer.getStringWidth(s1.substring(0, l3)) - this.minecraft.fontRenderer.getStringWidth(s1) / 2;
@@ -175,8 +196,6 @@ public class EditVegSignScreen extends EditSignScreen {
 
         matrixStack.pop();
         RenderHelper.setupGui3DDiffuseLighting();
-        for(int i1 = 0; i1 < this.buttons.size(); ++i1) {
-            this.buttons.get(i1).render(matrixStack, mouseX, mouseY, partialTicks);
-        }
+        super.render(matrixStack, mouseX, mouseY, partialTicks);
     }
 }
